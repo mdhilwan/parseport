273ba74085bdeb05f7d0b1e6b7b525f652bc468c
Md Hilwan <wan.wanted.1@gmail.com>
2024-01-11 07:17:20 -0600

    update super row and input to call the activate and deactivate user

--- app/admin/shared/input/input.js
+++ app/admin/shared/input/input.js
@@ -1,13 +1,16 @@
-const Input = ({colKey, value}) => {
-    if (typeof value === 'boolean' && value) {
+const Input = ({ colKey, value }) => {
+    console.log(colKey, value)
+    if (typeof value === 'boolean') {
+        const color = value ? 'blue' : 'red'
+        const text = value ? 'Active' : 'Inactive'
         return (
-            <span className="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">{colKey}</span>
+            <span className={`bg-${color}-100 text-${color}-800 text-xs font-medium px-2.5 py-0.5 rounded-full`}>{text}</span>
         )
     } if (value.type === 'password') {
         return (
-            <input 
-                type="password" 
-                aria-label="input" 
+            <input
+                type="password"
+                aria-label="input"
                 disabled
                 className="bg-gray-100 border border-gray-300 text-gray-900 rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2 py-1 text-xs" value={value} />
         )
@@ -20,7 +23,7 @@ const Input = ({colKey, value}) => {
                 className="bg-gray-100 border border-gray-300 text-gray-900 rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2 py-1 text-xs" value={value} />
         )
     }
-    
+
 }
 
 export default Input;
--- app/admin/super/row.js
+++ app/admin/super/row.js
@@ -1,24 +1,45 @@
+"use client"
+
 import utils from "@/app/utils"
 import Input from "@/app/admin/shared/input"
+import { HttpActions } from "@/app/api/httpActions"
+import { useState } from "react"
 
 const Row = ({ userIndex, userObject }) => {
+    const [user, setUser] = useState(userObject)
     return (
         <tr key={userIndex} className="bg-white border-b border-gray-200">
             <td className="whitespace-nowrap">
                 <button type="button" className="text-slate-700 bg-slate-300 hover:bg-slate-400 hover:border-slate-500 font-light rounded-s text-xs px-1.5 py-0.5 border border-slate-400">Edit</button>
                 <button type="button" className="text-slate-700 bg-slate-100 hover:bg-red-800 hover:text-slate-50 hover:border-red-900 font-light text-xs px-1.5 py-0.5 border border-slate-200">Del</button>
-                <button type="button" className="text-slate-700 bg-slate-100 hover:bg-red-800 hover:text-slate-50 hover:border-red-900 font-light rounded-e text-xs px-1.5 py-0.5 border border-slate-200">Deactivate</button>
+                <button type="button" className="text-slate-700 bg-slate-100 hover:bg-red-800 hover:text-slate-50 hover:border-red-900 font-light rounded-e text-xs px-1.5 py-0.5 border border-slate-200"
+                    onClick={async () => {
+                        const res = user.active ?
+                            await HttpActions.DeactivateUser(user.useremail) :
+                            await HttpActions.ActivateUser(user.useremail)
+                        if (res) {
+                            setUser({
+                                ...user,
+                                active: !user.active
+                            })
+                        }
+                    }}>
+                    {
+                        user.active ?
+                            "Deactivate" : "Activate"
+                    }
+                </button>
             </td>
-            {Object.entries(userObject).map(([colKey, value], colIndex) => {
+            {Object.entries(user).map(([colKey, value], colIndex) => {
                 const thisKey = utils.Rand8digit()
                 if (colIndex === 0) {
                     return <th key={thisKey} scope='row' className='font-medium text-gray-900 whitespace-nowrap'>
                         <Input value={value} colKey={colKey} />
                     </th>
                 } else
-                return <td key={thisKey} className="text-center">
-                    <Input value={value} colKey={colKey} />
-                </td>
+                    return <td key={thisKey} className="text-center">
+                        <Input value={value} colKey={colKey} />
+                    </td>
             })}
         </tr>
     )
