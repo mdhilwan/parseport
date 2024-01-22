f5026151bc42ae806c66a18a47281e0f31c6e197
Md Hilwan <wan.wanted.1@gmail.com>
2024-01-22 16:51:55 -0600

    allow all logins temporary

--- app/server/auth.js
+++ app/server/auth.js
@@ -21,36 +21,39 @@ export const authOptions = {
         async signIn({ user }) {
             const { res: { result } } = await HttpActions.GetUserByEmail(user.email)
             console.log("signIn:::::", result)
-            if (user.email && result.active) {
-                const { res: { result: { sessionid } } } = await HttpActions.UserLogin(user.email)
-                if (sessionid) {
+            // if (user.email && result.active) {
+            //     const { res: { result: { sessionid } } } = await HttpActions.UserLogin(user.email)
+            //     if (sessionid) {
                     return true
-                }
-                return false
-            }
-            return false
+            //     }
+            //     return false
+            // }
+            // return false
         },
         async session({ session }) {
             let { res: { result } } = await HttpActions.GetUserByEmail(session.user.email)
-            if (session.user.email && result.active) {
-                if (isSessionIdExpired(result.invaliddatetime)) {
-                    const { res: { result: { sessionid } } } = await HttpActions.RefreshSessionId(session.user.email)
-                    result.sessionid = sessionid
-                }
+            console.log("session:::::", result)
+            // if (session.user.email && result.active) {
+            //     if (isSessionIdExpired(result.invaliddatetime)) {
+            //         const { res: { result: { sessionid } } } = await HttpActions.RefreshSessionId(session.user.email)
+            //         result.sessionid = sessionid
+            //     }
 
-                return {
-                    ...session,
-                    sessionId: result.sessionid
-                }
-            }
-            return {}
+            //     return {
+            //         ...session,
+            //         sessionId: result.sessionid
+            //     }
+            // }
+            // return {}
+            return session
         },
         async jwt({ token }) {
             const { res: { result } } = await HttpActions.GetUserByEmail(token.email)
-            if (token.email && result.active) {
+            // console.log("jwt:::::", result)
+            // if (token.email && result.active) {
                 return token
-            }
-            return {}
+            // }
+            // return {}
         }
     },
     secret: process.env.NEXTAUTH_SECRET
