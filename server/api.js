eff8ae5b29c55c5c97ad93718078b7ddd2722d21
Md Hilwan <wan.wanted.1@gmail.com>
2024-01-22 15:47:45 -0600

    add log

--- server/api.js
+++ server/api.js
@@ -6,6 +6,8 @@ const express = require('express')
 const app = express();
 require('dotenv')
 
+console.log(process.env)
+
 const server = https.createServer({
     key: fs.readFileSync(process.env.WEBSOCKET_KEY),
     cert: fs.readFileSync(process.env.WEBSOCKET_CERT)
