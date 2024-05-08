fb7f6e6e6f0b067fc6fecd438fed6229cdf61684
Md Hilwan <wan.wanted.1@gmail.com>
2024-05-08 15:15:07 -0500

    track scans and download excel

--- app/page.js
+++ app/page.js
@@ -2,15 +2,21 @@ import AppLanding from './appLanding/appLanding'
 import '@/styles/global.css'
 import AuthGuard from './authGuard'
 import { getServerAuthSession } from './server/auth'
+import { HttpActions } from './api/httpActions'
 
 export default async function Home() {
   const authSession = await getServerAuthSession()
+  const userDetail = await HttpActions.GetUserByEmail(authSession.user.email)
 
   return (
     <AuthGuard whichAdmin="">
       {
         <main>
-          <AppLanding uuid={authSession?.sessionId} session={authSession} />
+          <AppLanding
+            uuid={authSession?.sessionId}
+            session={authSession}
+            user={userDetail}
+          />
         </main>
       }
     </AuthGuard>
