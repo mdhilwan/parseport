1ea9947e3e55a6bce312f64fddff6da67b38d9a3
Md Hilwan <wan.wanted.1@gmail.com>
2024-09-20 10:49:08 -0500

    mask passport details

--- app/parsedTable/input.tsx
+++ app/parsedTable/input.tsx
@@ -6,17 +6,19 @@ type InputType = {
   rowIndex: number
 }
 
+const maskData = (data: string) => data.replace(/\S(?=\S{2})/g, '*')
+
 const Input = (props: InputType) => {
   const { colIndex, rowIndex } = props
-  const { scannedData, highlightExpiredPassports } = useAppSelector(
-    (state) => state.mrzStore
-  )
+  const { scannedData, highlightExpiredPassports, maskPassportDetails } =
+    useAppSelector((state) => state.mrzStore)
   const dispatch = useAppDispatch()
 
   // @ts-ignore
   const data = scannedData[rowIndex][colIndex]
+
   const baseClassName =
-    'border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2 py-1 w-full h-8'
+    'border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2 py-1 w-full h-8 whitespace-nowrap'
   const isDateType = !isNaN(new Date(data).getTime())
   const defaultType = isDateType ? 'date' : 'text'
 
@@ -25,7 +27,9 @@ const Input = (props: InputType) => {
       ? new Date() > new Date(data)
       : false
 
-  return (
+  return maskPassportDetails ? (
+    <span className={baseClassName}>{maskData(data)}</span>
+  ) : (
     <input
       className={`${baseClassName} ${isDateExpired ? 'bg-amber-100 text-amber-800 border-amber-400' : ''}`}
       onChange={(evt) => {
