7aa2a7a49e9b6ba8d0b63c78b63cdee6f48f48d6
Md Hilwan <wan.wanted.1@gmail.com>
2024-11-01 11:23:59 -0500

    planned for generic tag vs umrah / hajj tags

--- app/generate/tag/generateTagPreview.tsx
+++ app/generate/tag/generateTagPreview.tsx
@@ -2,17 +2,29 @@ import BackBagTag from '@/app/generate/tag/bagTag/back'
 import FrontBagTag from '@/app/generate/tag/bagTag/front'
 
 const TagButtons = () => (
-  <div className="absolute top-4 left-4">
-    <TagButton label="Bag Tags" />
-    <TagButton label="Lanyards" />
+  <div className="absolute bottom-4 center">
+    <TagButton active={true} label="Luggage Tags" />
+    <TagButton disabled={true} label="Lanyards (coming soon)" />
   </div>
 )
 
-const TagButton = ({ label }: { label: string }) => {
+const PackageButtons = () => (
+  <div className="absolute top-4 center">
+    <TagButton active={true} label="Umrah / Makkah Tags" />
+    <TagButton disabled={true} label="Generic Tour Tags (coming soon)" />
+  </div>
+)
+
+type TagButtonType = { label: string; disabled?: boolean; active?: boolean }
+
+const TagButton = (tagButtonProp: TagButtonType) => {
+  const { label, disabled = false, active = false } = tagButtonProp
+  const baseClassName = `mt-3 mr-3 inline-flex w-full justify-center rounded-md ${active ? 'ring-gray-100 text-gray-200' : 'bg-gray-500 ring-gray-400 text-gray-400'} px-3 py-2 text-xs shadow-sm ring-1 ring-inset ${disabled || active ? '' : 'hover:bg-gray-50'} sm:mt-0 sm:w-auto`
   return (
     <button
       type="button"
-      className="mt-3 mr-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+      disabled={disabled}
+      className={baseClassName}
       onClick={() => {}}
     >
       {label}
@@ -22,10 +34,11 @@ const TagButton = ({ label }: { label: string }) => {
 export const GenerateTagPreview = () => {
   return (
     <div className="col-span-2">
-      <div className="bg-gray-500 rounded flex justify-center items-center h-[calc(100vh-16rem)] space-x-8 shadow-inner border border-gray-600 relative overflow-hidden">
+      <div className="bg-gray-500 rounded flex justify-center items-center h-[calc(100vh-16rem)] min-h-[calc(820px-10rem)] shadow-inner border border-gray-600 relative overflow-hidden">
         <TagButtons />
         <FrontBagTag />
         <BackBagTag />
+        <PackageButtons />
       </div>
     </div>
   )
