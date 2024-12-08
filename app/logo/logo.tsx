a0fbe7ec2331566f5b2c58c324d51f72eb7fa012
Md Hilwan <wan.wanted.1@gmail.com>
2024-12-08 14:19:59 -0700

    link the logo back to home page

--- app/logo/logo.tsx
+++ app/logo/logo.tsx
@@ -1,31 +1,33 @@
 const Logo = () => (
   <h1 className="flex items-center mb-12 text-2xl text-white font-normal">
-    Passport
-    <span className="ml-2 mr-1 whitespace-nowrap">
-      <svg
-        width="25"
-        height="21"
-        viewBox="0 0 25 21"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M2.59082 1.81824L11.3181 10.5455L2.59082 19.2728"
-          stroke="#F64E32"
-          stroke-width="3.27273"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M14.2275 1.81824L22.9548 10.5455L14.2275 19.2728"
-          stroke="#F64E32"
-          stroke-width="3.27273"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </span>
-    Visa
+    <a href="/" className="flex items-center">
+      Passport
+      <span className="ml-2 mr-1 whitespace-nowrap">
+        <svg
+          width="25"
+          height="21"
+          viewBox="0 0 25 21"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2.59082 1.81824L11.3181 10.5455L2.59082 19.2728"
+            stroke="#F64E32"
+            stroke-width="3.27273"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M14.2275 1.81824L22.9548 10.5455L14.2275 19.2728"
+            stroke="#F64E32"
+            stroke-width="3.27273"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </span>
+      Visa
+    </a>
   </h1>
 )
 
