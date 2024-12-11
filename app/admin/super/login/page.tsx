f55ab9e166edfdc39768e37c34d6bf596c3081ef
Md Hilwan <wan.wanted.1@gmail.com>
2024-12-10 23:24:22 -0700

    use the common login page

--- app/admin/super/login/page.tsx
+++ app/admin/super/login/page.tsx
@@ -1,25 +1,5 @@
-import { ControlSessionType } from '@/app/admin/shared/controls/controls'
-import Login from '@/app/admin/shared/login'
-import { WhichAdmin } from '@/app/enums/whichAdmin'
-import { getServerAuthSession } from '@/app/server/auth'
+import Unauth from '@/app/admin/shared/unauth'
 import '@/styles/global.css'
 
-const LoginSuper = async () => {
-  const authSession =
-    (await getServerAuthSession()) as unknown as ControlSessionType
-
-  return (
-    <section className="bg-gray-50">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
-        >
-          ParsePort: Super Admin
-        </a>
-        <Login whichAdmin={WhichAdmin.SUPER} session={authSession} />
-      </div>
-    </section>
-  )
-}
+const LoginSuper = () => <Unauth />
 export default LoginSuper
