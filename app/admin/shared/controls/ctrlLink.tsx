82b925ef522d87aa5866b2bf04b2b8e30d3ab1eb
Md Hilwan <wan.wanted.1@gmail.com>
2024-12-10 23:51:23 -0700

    add active state to links

--- app/admin/shared/controls/ctrlLink.tsx
+++ app/admin/shared/controls/ctrlLink.tsx
@@ -1,3 +1,4 @@
+import { usePathname } from 'next/navigation'
 import { MouseEventHandler } from 'react'
 
 export type ControlLinkType = {
@@ -6,18 +7,30 @@ export type ControlLinkType = {
   clickEvent?: MouseEventHandler
 }
 
+const textToUrlMap: { [key: string]: string } = {
+  '/': 'Passports',
+  '/admin': 'Settings',
+  '/admin/super': 'Super',
+  '/admin/super/add': 'Add Account',
+}
+
 const CtrlLink = (props: ControlLinkType) => {
   const { text, url = '', clickEvent } = props
+  const pathname = usePathname()
+  const isActive = textToUrlMap[pathname] === text
 
   const baseProps = {
     'data-testid': 'ControlLink',
     type: 'button',
-    className:
-      'text-base-color py-6 hover:cursor-pointer font-light text-sm mx-4 first-of-type:ms-0 last-of-type:me-0 py-0.5',
   }
 
   return (
-    <a {...baseProps} href={url} onClick={clickEvent}>
+    <a
+      className={`text-base-color py-6 hover:cursor-pointer font-light text-sm mx-4 first-of-type:ms-0 last-of-type:me-0 ${isActive ? 'border-b-2 border-b-black pb-[22px]' : ''}`}
+      {...baseProps}
+      href={url}
+      onClick={clickEvent}
+    >
       {text}
     </a>
   )
