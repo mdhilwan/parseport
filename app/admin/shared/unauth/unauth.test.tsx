0bf1c5d8615139245a1d91b13d69e9005bccf77f
Md Hilwan <wan.wanted.1@gmail.com>
2024-12-10 23:21:21 -0700

    fixed test

--- app/admin/shared/unauth/unauth.test.tsx
+++ app/admin/shared/unauth/unauth.test.tsx
@@ -3,7 +3,6 @@ import { WhichAdmin } from '@/app/enums/whichAdmin'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
-// Mock the Login component
 // eslint-disable-next-line react/display-name
 jest.mock('../login', () => ({ whichAdmin }: { whichAdmin: WhichAdmin }) => (
   <div data-testid="LoginMock">Login Component - {whichAdmin}</div>
@@ -13,33 +12,18 @@ describe('Unauth component', () => {
   it('renders the Unauth component', () => {
     render(<Unauth />)
 
-    // Check that the Unauth component renders correctly
-    const sectionElement = screen.getByTestId('Unauth')
+    const sectionElement = screen.getByTestId('unauth')
     expect(sectionElement).toBeInTheDocument()
-
-    // Check default heading text (without whichAdmin prop)
-    const headingElement = screen.getByText('Passport to Visa')
-    expect(headingElement).toBeInTheDocument()
-
-    // Check "You are not logged in" text
-    const notLoggedInText = screen.getByText('You are not logged in')
-    expect(notLoggedInText).toBeInTheDocument()
   })
 
   it('renders the correct heading for super admin', () => {
     render(<Unauth whichAdmin={WhichAdmin.SUPER} />)
-
-    // Check that the heading includes ": Super Admin"
-    const headingElement = screen.getByText('Passport to Visa')
-    expect(headingElement).toBeInTheDocument()
+    expect(screen.getByTestId('unauth-super')).toBeInTheDocument()
   })
 
   it('renders the correct heading for regular admin', () => {
     render(<Unauth whichAdmin={WhichAdmin.ADMIN} />)
-
-    // Check that the heading includes ": Admin"
-    const headingElement = screen.getByText('Passport to Visa')
-    expect(headingElement).toBeInTheDocument()
+    expect(screen.getByTestId('unauth-admin')).toBeInTheDocument()
   })
 
   it('passes the correct whichAdmin prop to the Login component', () => {
