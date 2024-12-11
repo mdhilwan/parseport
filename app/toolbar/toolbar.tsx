1e9512a65e7119156d9223374116e8452759d53f
Md Hilwan <wan.wanted.1@gmail.com>
2024-12-10 23:23:02 -0700

    move component into the control component

--- app/toolbar/toolbar.tsx
+++ app/toolbar/toolbar.tsx
@@ -2,13 +2,11 @@ import { GenerateCsvButton } from '@/app/generate/csv'
 import HighlightExpiredPassports from '@/app/highlightExpiredPassports'
 import MaskDetails from '@/app/maskDetails'
 import ScanNotification from '@/app/scanNotification'
-import StatePhoneConnection from '@/app/statePhoneConnection'
 
 const Toolbar = () => {
   return (
     <div className="flex items-center py-4 fixed bottom-0">
       <MaskDetails />
-      <StatePhoneConnection />
       <GenerateCsvButton />
       <HighlightExpiredPassports />
       <ScanNotification />
