45c897f8acaccee291de3c3759093e8b084ef0bf
Md Hilwan <wan.wanted.1@gmail.com>
2025-01-02 23:24:04 -0600

    hard code col-span-3 into global.scss

--- app/stateMrzInput/baseCta.tsx
+++ app/stateMrzInput/baseCta.tsx
@@ -24,27 +24,24 @@ export const BaseCta = (prop: BaseCta) => {
     modifierClass = undefined,
   } = prop
 
-  const baseProp: { className: string; onClick?: () => void } = {
-    className: `col-span-${colSpan} p-4 border border-gray-400 rounded-xl text-center transition-all hover:border-gray-600 hover:shadow-md`,
-  }
-
-  if (colStart) {
-    baseProp.className = `col-start-${colStart} ${baseProp.className}`
-  }
-
-  if (modifierClass) {
-    baseProp.className = `${baseProp.className} ${modifierClass}`
-  }
-
-  if (onClick) {
-    baseProp.onClick = onClick
-  }
-
   switch (type) {
     case CtaType.Button:
-      baseProp.className += ' hover:cursor-pointer'
-      return <button {...baseProp}>{children}</button>
+      return (
+        <button
+          onClick={onClick}
+          className={`${colStart ? `col-start-${colStart}` : ''} col-span-${colSpan} p-4 border border-gray-400 rounded-xl text-center transition-all hover:border-gray-600 hover:shadow-md hover:cursor-pointer ${modifierClass}`}
+        >
+          {children}
+        </button>
+      )
     case CtaType.Div:
-      return <div {...baseProp}>{children}</div>
+      return (
+        <div
+          onClick={onClick}
+          className={`${colStart ? `col-start-${colStart}` : ''} col-span-${colSpan} p-4 border border-gray-400 rounded-xl text-center transition-all hover:border-gray-600 hover:shadow-md ${modifierClass}`}
+        >
+          {children}
+        </div>
+      )
   }
 }
