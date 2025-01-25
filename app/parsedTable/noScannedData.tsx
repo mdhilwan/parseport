ae8c9178c7026fa1f49a7cb85df9985b2f409815
Md Hilwan <wan.wanted.1@gmail.com>
2025-01-24 23:02:13 -0600

    if there's no scanned data, still show mrz input

--- app/parsedTable/noScannedData.tsx
+++ app/parsedTable/noScannedData.tsx
@@ -1,21 +1,12 @@
 import { UserType } from '@/app/admin/shared/controls/controls'
 import StateMrzInput from '@/app/stateMrzInput'
-import { useAppSelector } from '@/app/store'
 
 export const NoScannedData = ({ user }: { user: UserType }) => {
-  const { disconnected } = useAppSelector((state) => state.mrzStore)
   return (
     <div className="text-slate-500">
-      {disconnected ? (
-        <div className="my-5">
-          <StateMrzInput user={user} />
-        </div>
-      ) : (
-        <>
-          Use your phone that you have linked this computer with to scan your
-          document
-        </>
-      )}
+      <div className="my-5">
+        <StateMrzInput user={user} />
+      </div>
     </div>
   )
 }
