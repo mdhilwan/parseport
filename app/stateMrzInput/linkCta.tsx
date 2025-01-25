e7e161d13705d9534a7d660de839d89cfde80afb
Md Hilwan <wan.wanted.1@gmail.com>
2025-01-24 23:19:18 -0600

    new text if phone is linked

--- app/stateMrzInput/linkCta.tsx
+++ app/stateMrzInput/linkCta.tsx
@@ -1,17 +1,31 @@
 import { setShowQrCodeModal } from '@/app/slice/slice'
 import { BaseCta, CtaType } from '@/app/stateMrzInput/baseCta'
+import { useAppSelector } from '@/app/store'
 import { useDispatch } from 'react-redux'
 
 export const LinkCta = () => {
   const dispatch = useDispatch()
+  const { disconnected } = useAppSelector((state) => state.mrzStore)
   return (
-    <BaseCta
-      type={CtaType.Button}
-      colSpan={3}
-      colStart={4}
-      onClick={() => dispatch(setShowQrCodeModal(true))}
-    >
-      <span className={'text-base-brand'}>Link your phone with a QR code</span>
-    </BaseCta>
+    <>
+      {disconnected ? (
+        <BaseCta
+          type={CtaType.Button}
+          colSpan={3}
+          colStart={4}
+          onClick={() => dispatch(setShowQrCodeModal(true))}
+        >
+          <span className={'text-base-brand'}>
+            Link your phone with a QR code
+          </span>
+        </BaseCta>
+      ) : (
+        <BaseCta type={CtaType.Div} colSpan={3} colStart={4}>
+          <span className={'text-base-brand'}>
+            Scan the document with your phone
+          </span>
+        </BaseCta>
+      )}
+    </>
   )
 }
